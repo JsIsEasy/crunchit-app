e33cdce3d96dd0296182c4c4e50ec4abd2067b93
karannakra <karannakra1@gmail.com>
2025-09-21 12:31:08 +0530

    Improve metadata tag for site

---
+++ storefront/src/app/metadata/icons.ts
@@ -0,0 +1,8 @@
+import type { Metadata } from "next";
+
+export const iconsMetaData: Metadata =  {
+    icons: {
+        icon:'/icons/icon.ico',
+        apple:'icons/apple-touch-icon.png',
+    },
+};

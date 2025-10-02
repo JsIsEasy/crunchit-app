ed3bc1c957efac70b04b9e8753763542c83f3048
karannakra <karannakra1@gmail.com>
2025-10-02 16:58:13 +0530

    Setup container compose for mysql and init with first table

---
+++ db/init.sql
@@ -0,0 +1,19 @@
+-- -----------------------------------------------------
+-- Table `jobs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `job_id` VARCHAR(36) NOT NULL,
+  `file_name` VARCHAR(255) NOT NULL,
+  `s3_key` VARCHAR(255) NOT NULL,
+  `compressed_s3_key` VARCHAR(255) NULL,
+  `current_status` VARCHAR(50) NOT NULL,
+
+  `started_at` DATETIME NULL,
+  `finished_at` DATETIME NULL,
+  
+  -- The field you wanted auto-created:
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  
+  PRIMARY KEY (`job_id`),
+  INDEX `idx_current_status` (`current_status` ASC)
+);
